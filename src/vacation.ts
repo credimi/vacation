42b463ffde68ad084b84934be6363502c052b779
Davide Di Pumpo <davide.dipumpo@credimi.com>
2019-07-30 18:12:54 +0200

    adds prettier

--- src/vacation.ts
+++ src/vacation.ts
@@ -1,65 +1,65 @@
-import { holidays } from "../data/holidays";
-import { vacations } from "../data/vacations";
+import { holidays } from '../data/holidays'
+import { vacations } from '../data/vacations'
 
-export { holidays, vacations };
+export { holidays, vacations }
 
 type Options = {
-  withVacancy?: Boolean;
-  withHolidays?: Boolean;
-  withLastWorkDayOfMonth?: Boolean;
-  withWeekends?: Boolean;
-};
+  withVacancy?: Boolean
+  withHolidays?: Boolean
+  withLastWorkDayOfMonth?: Boolean
+  withWeekends?: Boolean
+}
 
 const defaultOptions: Options = {
   withVacancy: true,
   withHolidays: true,
   withLastWorkDayOfMonth: false,
-  withWeekends: true
-};
+  withWeekends: true,
+}
 
 const minusDay = (date: Date, days: number = 1) =>
-  new Date(new Date(date).setDate(date.getDate() - days));
+  new Date(new Date(date).setDate(date.getDate() - days))
 
 export const getLastWorkDayOfMonth = (
   year: number,
   month: number,
   options?: Options
 ) => {
-  let date = new Date(Date.UTC(year, month, 0));
+  let date = new Date(Date.UTC(year, month, 0))
 
   while (isVacation(date, { ...options, withLastWorkDayOfMonth: false })) {
-    date = minusDay(date);
+    date = minusDay(date)
   }
 
-  return date.toISOString().split("T")[0];
-};
+  return date.toISOString().split('T')[0]
+}
 
 export const isVacation = (date: Date, proposedOptions?: Options): Boolean => {
   const { withWeekends, withVacancy, withHolidays, withLastWorkDayOfMonth } = {
     ...defaultOptions,
-    ...proposedOptions
-  };
-
-  if (withWeekends && (date.getDay() === 6 || date.getDay() === 0)) {
-    return true;
+    ...proposedOptions,
   }
 
-  const dateString = date.toISOString().split("T")[0];
+  if (withWeekends && (date.getDay() === 6 || date.getDay() === 0)) {
+    return true
+  }
+
+  const dateString = date.toISOString().split('T')[0]
 
   if (withVacancy && vacations.includes(dateString)) {
-    return true;
+    return true
   }
 
   if (withHolidays && holidays.includes(dateString)) {
-    return true;
+    return true
   }
 
   if (withLastWorkDayOfMonth) {
-    const last = getLastWorkDayOfMonth(date.getFullYear(), date.getMonth() + 1);
-    if (date.toISOString().split("T")[0] === last) {
-      return true;
+    const last = getLastWorkDayOfMonth(date.getFullYear(), date.getMonth() + 1)
+    if (date.toISOString().split('T')[0] === last) {
+      return true
     }
   }
 
-  return false;
-};
+  return false
+}
--- utils/makesHolidays.js
+++ utils/makesHolidays.js
@@ -4,13 +4,14 @@ const fsPromises = require('fs.promises')
 
 let holyarray = []
 for (let year = 2019; year < 2030; year++) {
-    holyarray = [...holyarray, ...holidays.getHolidays(year).map(({end}) => 
-        end.toISOString().split('T')[0])]
+  holyarray = [...holyarray, ...holidays.getHolidays(year).map(({
+      end
+    }) =>
+    end.toISOString().split('T')[0])]
 }
 
 fsPromises.writeFile('./data/holidays.ts', `
 export const holidays: string[] = [${holyarray.map(holiday => `
-    "${holiday}"`)}
+  '${holiday}'`)}
 ]
 `)
-
