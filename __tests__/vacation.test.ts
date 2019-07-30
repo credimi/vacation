4997760e4ad73f258dfcfd337f21f64e16e2dc74
Davide Di Pumpo <davide.dipumpo@credimi.com>
2019-07-30 17:53:02 +0200

    rename files and functions

---
+++ __tests__/vacation.test.ts
@@ -0,0 +1,55 @@
+import { isVacation, getLastWorkDayOfMonth } from "../src/vacation";
+
+test("Christmas is CHRISTMAS", () => {
+  const options = {
+    withWeekends: false,
+    withVacancy: false
+  };
+  expect(isVacation(new Date("2020-12-25"), options)).toBe(true);
+  expect(isVacation(new Date("2020-12-24"), options)).toBe(false);
+});
+
+test("Christmas is CHRISTMAS, but not if you don't ask", () => {
+  const options = {
+    withWeekends: false,
+    withVacancy: false,
+    withHolidays: false
+  };
+  expect(isVacation(new Date("2020-12-25"), options)).toBe(false);
+});
+
+test("It's weekend", () => {
+  const options = {
+    withVacancy: false,
+    withHolidays: false
+  };
+  expect(isVacation(new Date("2023-07-29"), options)).toBe(true);
+  expect(isVacation(new Date("2023-07-30"), options)).toBe(true);
+  expect(isVacation(new Date("2023-07-28"), options)).toBe(false);
+});
+
+test("It's vacancy", () => {
+  const options = {
+    withWeekends: false,
+    withVacancy: true,
+    withHolidays: false
+  };
+  expect(isVacation(new Date("2019-07-12"), options)).toBe(true);
+  expect(isVacation(new Date("2023-07-11"), options)).toBe(false);
+  expect(isVacation(new Date("2019-12-31"), options)).toBe(true);
+});
+
+test("Last working day", () => {
+  const options = {
+    withLastWorkDayOfMonth: true
+  };
+  expect(isVacation(new Date("2019-07-31"), options)).toBe(true);
+  expect(isVacation(new Date("2019-07-30"), options)).toBe(false);
+  expect(isVacation(new Date("2019-12-30"), options)).toBe(true);
+  expect(isVacation(new Date("2019-12-27"), options)).toBe(false);
+});
+
+test("Get last working date", () => {
+  expect(getLastWorkDayOfMonth(2019, 7)).toBe("2019-07-31");
+  expect(getLastWorkDayOfMonth(2019, 12)).toBe("2019-12-30");
+});
