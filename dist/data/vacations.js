fde081276cd1bc648c5126a4e70ee9b585b9aef1
Claudia Afonso <claudia.afonso@credimi.com>
2023-03-08 14:15:06 +0100

    readds dist folder width build files

---
+++ dist/data/vacations.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.vacations = void 0;
+exports.vacations = [
+    '2019-07-12',
+    '2019-08-14',
+    '2019-08-16',
+    '2019-12-31',
+    '2019-09-16'
+];
